887566c6254f2eba21181cd4e0b0e046bf1a2eae
nico3117 <nicobaca17@gmail.com>
2023-01-10 08:00:19 +0100

    google map api

---
+++ src/components/map.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+
+function MapGoogle() {
+
+  const [Circuit, setCircuit] = React.useState([])
+  
+  React.useEffect(() => {
+    fetch("https://ergast.com/api/f1/current/circuits.json")
+    .then((res) => { return res.json() })
+    .then((data) => {   
+        setCircuit(data.MRData.CircuitTable.Circuits);
+    })
+    .catch((error) => { return console.log('error', error) });
+  }, [])
+
+  const containerStyle = {
+    width: '800px',
+    height: '800px'
+  };
+  
+  const center = {
+    lat: 46.63728,
+    lng: 2.3382623
+  };
+
+  const api_key = process.env.REACT_APP_API_GOOGLE_MAP;
+  
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: api_key
+  })
+
+  return isLoaded ? (
+    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={4}>
+      {
+        Circuit.map(circuit => {     
+          <Marker key={circuit.circuitId} position={{
+            lat: parseFloat(circuit.Location.lat), 
+            lng: parseFloat(circuit.Location.long)
+          }} />
+        })
+      }
+    </GoogleMap>
+  ) : <></>
+}
+
+export default MapGoogle
--- src/views/home.js
+++ src/views/home.js
@@ -1,46 +1,23 @@
 import React from 'react'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-import L from 'leaflet';
-import icon from 'leaflet/dist/images/marker-icon.png';
+import Map from '../components/map'
 
 function Home() {
 
-    const [Circuit, setCircuit] = React.useState([])
+    // const [Circuit, setCircuit] = React.useState([])
 
-    React.useEffect(() => {
-        fetch("https://ergast.com/api/f1/current/circuits.json")
-        .then((res) => { return res.json() })
-        .then((data) => {   
-            setCircuit(data.MRData.CircuitTable.Circuits);
-        })
-        .catch((error) => { return console.log('error', error) });
-      }, [])
+    // React.useEffect(() => {
+    //     fetch("https://ergast.com/api/f1/current/circuits.json")
+    //     .then((res) => { return res.json() })
+    //     .then((data) => {   
+    //         setCircuit(data.MRData.CircuitTable.Circuits);
+    //     })
+    //     .catch((error) => { return console.log('error', error) });
+    //   }, [])
 
-    let DefaultIcon = L.icon({
-        iconUrl: icon,
-        iconSize: [24,36],
-        iconAnchor: [12,36]
-    });
-
-    L.Marker.prototype.options.icon = DefaultIcon;
 
     return (
         <div className="container">
-            <MapContainer style={{ width: "900px", height: "600px" }} className="mx-auto mt-10" center={[46.63728, 2.3382623]} zoom={4}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                />
-                {
-                    Circuit.map(circuit => 
-                        <Marker position={[circuit.Location.lat, circuit.Location.long]}>
-                            <Popup>
-                                {circuit.circuitName}
-                            </Popup>
-                        </Marker>
-                    )
-                }
-            </MapContainer>
+            <Map />
         </div>
     )
 }
